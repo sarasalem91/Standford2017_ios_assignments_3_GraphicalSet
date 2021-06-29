877f14a84ce15befa891dc5404585d02caecc213
SaraESalem <dev.sarasalem@gmail.com>
2021-06-29 15:03:35 +0200

    uup

--- SetSara/View/CardView.swift
+++ SetSara/View/CardView.swift
@@ -11,13 +11,13 @@ import UIKit
 @IBDesignable
 class CardView: UIView {
     @IBInspectable
-    var shape : String = "oval" {didSet{setNeedsDisplay()}}
+    var shape : String = "squiggle" {didSet{setNeedsDisplay()}}
     @IBInspectable
-    var number : CGFloat = 3 {didSet{setNeedsDisplay()}}
+    var number : CGFloat = 2 {didSet{setNeedsDisplay()}}
     @IBInspectable
     var color : UIColor = UIColor.red {didSet{setNeedsDisplay()}}
     @IBInspectable
-    var shading : String = "striped" {didSet{setNeedsDisplay()}}
+    var shading : String = "unfilled" {didSet{setNeedsDisplay()}}
     
     var constraint_distance : CGFloat = 16
     
@@ -58,9 +58,206 @@ class CardView: UIView {
     
     func draw_squiggle(){
         let path = UIBezierPath()
-        path.addClip()
         
-        
+        if number == 1{
+            
+            path.move(to: CGPoint(x: 2*constraint_distance, y: bounds.height/4))
+            let cp1 =  CGPoint(x: bounds.width/2, y: 0)
+            let cp2 =  CGPoint(x: bounds.width/2, y: 0.75*bounds.height)
+            
+            path.addCurve(to: CGPoint(x: (bounds.width-2*constraint_distance), y: bounds.height/4), controlPoint1: cp1, controlPoint2: cp2)
+            
+            path.addQuadCurve(to: CGPoint(x: bounds.width-3*constraint_distance, y: 0.75*bounds.height), controlPoint:CGPoint(x: bounds.maxX, y:bounds.midY))
+            
+            let cp11 =  CGPoint(x: bounds.width/2, y: bounds.maxY)
+            let cp22 =  CGPoint(x: bounds.width/2, y: bounds.height/4)
+            path.addCurve(to: CGPoint(x: (2*constraint_distance), y: 0.75*bounds.height), controlPoint1: cp11, controlPoint2: cp22)
+            
+            
+            path.addQuadCurve(to: CGPoint(x: 2*constraint_distance, y: bounds.height/4), controlPoint:CGPoint(x: bounds.minX, y: bounds.midY))
+            
+            
+            
+            path.lineWidth = 3
+            
+            if shading ==  "solid"{
+                color.setFill()
+                path.fill()
+            }else if shading ==  "striped"{
+                
+                var const_distance:CGFloat = 10
+                var distanceBetween_stripped_lines:CGFloat = const_distance
+                var looping_distance:CGFloat = bounds.width - 2*constraint_distance
+                
+                for x in 0...Int(looping_distance/distanceBetween_stripped_lines){
+                    
+                    path.move(to: CGPoint(x: (constraint_distance + distanceBetween_stripped_lines), y: 0))
+                    path.addLine(to: CGPoint(x: (constraint_distance + distanceBetween_stripped_lines), y: bounds.maxY ))
+                    
+                    distanceBetween_stripped_lines += const_distance
+                }
+                
+                
+                path.addClip()
+                color.setStroke()
+                path.stroke()
+                
+                
+            }else{ //unfilled
+                color.setStroke()
+                path.stroke()
+            }
+            
+        }else  if number == 2{
+          
+            //first
+            path.move(to: CGPoint(x: 2*constraint_distance, y: bounds.height/8))
+            let cp1 =  CGPoint(x: bounds.width/2, y: 0)
+            let cp2 =  CGPoint(x: bounds.width/2, y: (3/8)*bounds.height)
+            
+            path.addCurve(to: CGPoint(x: (bounds.width-2*constraint_distance), y: bounds.height/8), controlPoint1: cp1, controlPoint2: cp2)
+            
+            path.addQuadCurve(to: CGPoint(x: bounds.width-3*constraint_distance, y: (3/8)*bounds.height), controlPoint:CGPoint(x: bounds.maxX, y:bounds.height/8))
+            
+            let cp11 =  CGPoint(x: bounds.width/2, y: bounds.height/2)
+            let cp22 =  CGPoint(x: bounds.width/2, y: bounds.height/8)
+            path.addCurve(to: CGPoint(x: (2*constraint_distance), y: (3/8)*bounds.height), controlPoint1: cp11, controlPoint2: cp22)
+            
+            path.addQuadCurve(to: CGPoint(x: 2*constraint_distance, y: bounds.height/8), controlPoint:CGPoint(x: bounds.minX, y: (3/8)*bounds.height))
+            
+            
+            //second
+            path.move(to: CGPoint(x: 2*constraint_distance, y: bounds.height*(5/8)))
+            let scp1 =  CGPoint(x: bounds.width/2, y: bounds.height/2)
+            let scp2 =  CGPoint(x: bounds.width/2, y: bounds.height)
+            
+            path.addCurve(to: CGPoint(x: (bounds.width-2*constraint_distance), y: bounds.height*(5/8)), controlPoint1: scp1, controlPoint2: scp2)
+            
+            path.addQuadCurve(to: CGPoint(x: bounds.width-3*constraint_distance, y: bounds.height*(7/8)), controlPoint:CGPoint(x: bounds.maxX, y:bounds.height*(3/4)))
+            
+            let scp11 =  CGPoint(x: bounds.width/2, y: bounds.maxY)
+            let scp22 =  CGPoint(x: bounds.width/2, y: (3/4)*bounds.height)
+            path.addCurve(to: CGPoint(x: (2*constraint_distance), y: (7/8)*bounds.height), controlPoint1: scp11, controlPoint2: scp22)
+            
+            path.addQuadCurve(to: CGPoint(x: 2*constraint_distance, y: (5/8)*bounds.height), controlPoint:CGPoint(x: bounds.minX, y: bounds.height*(3/4)))
+            
+            
+            
+            
+            path.lineWidth = 3
+            
+            if shading ==  "solid"{
+                color.setFill()
+                path.fill()
+            }else if shading ==  "striped"{
+                
+                var const_distance:CGFloat = 10
+                var distanceBetween_stripped_lines:CGFloat = const_distance
+                var looping_distance:CGFloat = bounds.width - 2*constraint_distance
+                
+                for x in 0...Int(looping_distance/distanceBetween_stripped_lines){
+                    
+                    path.move(to: CGPoint(x: (constraint_distance + distanceBetween_stripped_lines), y: 0))
+                    path.addLine(to: CGPoint(x: (constraint_distance + distanceBetween_stripped_lines), y: bounds.maxY ))
+                    
+                    distanceBetween_stripped_lines += const_distance
+                }
+                
+                
+                path.addClip()
+                color.setStroke()
+                path.stroke()
+                
+                
+            }else{ //unfilled
+                color.setStroke()
+                path.stroke()
+            }
+            
+        }else  if number == 3{
+            
+            //first
+            path.move(to: CGPoint(x: 2*constraint_distance, y: bounds.height/12))
+            let cp1 =  CGPoint(x: bounds.width/2, y: 0)
+            let cp2 =  CGPoint(x: bounds.width/2, y: (1/3)*bounds.height)
+            
+            path.addCurve(to: CGPoint(x: (bounds.width-2*constraint_distance), y: bounds.height/12), controlPoint1: cp1, controlPoint2: cp2)
+            
+            path.addQuadCurve(to: CGPoint(x: bounds.width-3*constraint_distance, y: (1/4)*bounds.height), controlPoint:CGPoint(x: bounds.maxX, y:bounds.height/6))
+            
+            let cp11 =  CGPoint(x: bounds.width/2, y: bounds.height/3)
+            let cp22 =  CGPoint(x: bounds.width/2, y: 0)
+            path.addCurve(to: CGPoint(x: (2*constraint_distance), y: (1/4)*bounds.height), controlPoint1: cp11, controlPoint2: cp22)
+            
+            path.addQuadCurve(to: CGPoint(x: 2*constraint_distance, y: bounds.height/12), controlPoint:CGPoint(x: bounds.minX, y: (1/6)*bounds.height))
+            
+            
+            //second
+            path.move(to: CGPoint(x: 2*constraint_distance, y: bounds.height*(5/12)))
+            let scp1 =  CGPoint(x: bounds.width/2, y: bounds.height/3)
+            let scp2 =  CGPoint(x: bounds.width/2, y: bounds.height*(2/3))
+
+            path.addCurve(to: CGPoint(x: (bounds.width-2*constraint_distance), y: bounds.height*(5/12)), controlPoint1: scp1, controlPoint2: scp2)
+
+            path.addQuadCurve(to: CGPoint(x: bounds.width-3*constraint_distance, y: bounds.height*(7/12)), controlPoint:CGPoint(x: bounds.maxX, y:bounds.height*(1/2)))
+
+            let scp11 =  CGPoint(x: bounds.width/2, y: (2/3)*bounds.height)
+            let scp22 =  CGPoint(x: bounds.width/2, y: (1/3)*bounds.height)
+            path.addCurve(to: CGPoint(x: (2*constraint_distance), y: (7/12)*bounds.height), controlPoint1: scp11, controlPoint2: scp22)
+
+            path.addQuadCurve(to: CGPoint(x: 2*constraint_distance, y: (5/12)*bounds.height), controlPoint:CGPoint(x: bounds.minX, y: bounds.height*(1/2)))
+            
+            
+            //third
+            path.move(to: CGPoint(x: 2*constraint_distance, y: bounds.height*(9/12)))
+            let scp13 =  CGPoint(x: bounds.width/2, y: bounds.height*(2/3))
+            let scp23 =  CGPoint(x: bounds.width/2, y: bounds.height)
+
+            path.addCurve(to: CGPoint(x: (bounds.width-2*constraint_distance), y: bounds.height*(9/12)), controlPoint1: scp13, controlPoint2: scp23)
+
+            path.addQuadCurve(to: CGPoint(x: bounds.width-3*constraint_distance, y: bounds.height*(11/12)), controlPoint:CGPoint(x: bounds.maxX, y:bounds.height*(5/6)))
+
+            let scp113 =  CGPoint(x: bounds.width/2, y: bounds.height)
+            let scp223 =  CGPoint(x: bounds.width/2, y: (2/3)*bounds.height)
+            path.addCurve(to: CGPoint(x: (2*constraint_distance), y: (11/12)*bounds.height), controlPoint1: scp113, controlPoint2: scp223)
+
+            path.addQuadCurve(to: CGPoint(x: 2*constraint_distance, y: (9/12)*bounds.height), controlPoint:CGPoint(x: bounds.minX, y: bounds.height*(5/6)))
+            
+            
+            
+            
+            
+            path.lineWidth = 3
+            
+            if shading ==  "solid"{
+                color.setFill()
+                path.fill()
+            }else if shading ==  "striped"{
+                
+                var const_distance:CGFloat = 10
+                var distanceBetween_stripped_lines:CGFloat = const_distance
+                var looping_distance:CGFloat = bounds.width - 2*constraint_distance
+                
+                for x in 0...Int(looping_distance/distanceBetween_stripped_lines){
+                    
+                    path.move(to: CGPoint(x: (constraint_distance + distanceBetween_stripped_lines), y: 0))
+                    path.addLine(to: CGPoint(x: (constraint_distance + distanceBetween_stripped_lines), y: bounds.maxY ))
+                    
+                    distanceBetween_stripped_lines += const_distance
+                }
+                
+                
+                path.addClip()
+                color.setStroke()
+                path.stroke()
+                
+                
+            }else{ //unfilled
+                color.setStroke()
+                path.stroke()
+            }
+            
+        }
     }
     func draw_oval(){
         let path = UIBezierPath()
